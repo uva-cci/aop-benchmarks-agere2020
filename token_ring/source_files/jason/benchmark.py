6f72fbf68f89d82e48c9564f3501329f61acc76e
Giovanni Sileno <g.sileno@uva.nl>
2020-08-11 16:07:44 +0200

    added meta parameters in the benchmark scripts

--- token_ring/source_files/jason/benchmark.py
+++ token_ring/source_files/jason/benchmark.py
@@ -3,9 +3,52 @@ import subprocess
 import psutil
 import time
 
-start = time.time_ns()
-psutil.cpu_percent(interval=0, percpu=True)
-subprocess.run(["/home/msotafa/IdeaProjects/jason/scripts/jason","/home/msotafa/IdeaProjects/jason/examples/ring/threadring.mas2j"])
-print(psutil.cpu_percent(interval=0, percpu=True))
-end = time.time_ns()
-print ((end - start) / (10 ** 9 ))
+import glob, os
+
+# jason should be in the path: e.g.
+#
+# export JASON_HOME=$HOME/opt/jason
+# export PATH=$JASON_HOME/scripts:$PATH
+
+def make_dir(path):
+	if not os.path.isdir(path):
+		try:
+			os.mkdir(path)
+		except OSError:
+			raise RuntimeError("Creation of the directory %s failed" % path)
+
+
+def generate_meta(nbagents, nbtokens):
+	os.chdir("./")
+	output_path = "W%s_T%s" % (nbagents, nbtokens)
+
+	make_dir(output_path)
+
+	asl_files = glob.glob("*.asl.meta")
+	mas2j_files = glob.glob("*.mas2j.meta")
+
+	for file in asl_files + mas2j_files:
+		fin = open(file, "rt")
+		fout = open(output_path + "/" + file.replace(".meta", ""), "wt")
+		for line in fin:
+			fout.write(line.replace('__NBAGENTS__', str(nbagents)).replace('__NBTOKENS__', str(nbtokens)))
+		fin.close()
+		fout.close()
+
+
+def run_test(filename):
+	if not filename.endswith(".mas2j"):
+		print("Wrong filename: %s" % filename)
+	else: 
+		start = time.time_ns()
+		psutil.cpu_percent(interval=0, percpu=True)
+		subprocess.run(["/Users/giovanni/opt/jason/scripts/jason", filename])
+		print(psutil.cpu_percent(interval=0, percpu=True))
+		end = time.time_ns()
+		print ((end - start) / (10 ** 9 ))
+
+
+nbagents = 2
+nbtokens = 250
+generate_meta(nbagents, nbtokens)
+run_test("W%s_T%s/threadring_with_distributor.mas2j" % (str(nbagents), str(nbtokens)))
